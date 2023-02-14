9de916ab75ee2eae3850ea86098e019e4148e5a8
cedricquitor <cedricquitor.dev@gmail.com>
2023-02-15 06:49:15 +0800

    server: Added post route to add new systems

--- server/routes/systemsRoutes.js
+++ server/routes/systemsRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../db");
+const { v4: uuidv4 } = require("uuid");
 
 // Get all systems
 router.route("/:userEmail").get(async (req, res) => {
@@ -12,6 +13,18 @@ router.route("/:userEmail").get(async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).send("Server error");
+  }
+});
+
+// Add a new system
+router.route("/").post(async (req, res) => {
+  console.log(req.body);
+  const id = uuidv4();
+  const { user_email, title, progress } = req.body;
+  try {
+    pool.query(`INSERT INTO systems(id, user_email, title, progress) VALUES($1, $2, $3, $4)`, [id, user_email, title, progress]);
+  } catch (error) {
+    console.error(error);
   }
 });
 
