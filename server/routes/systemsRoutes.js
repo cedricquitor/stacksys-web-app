d2fcc24f491e412a391f60be72eb6d28d4fe1254
cedricquitor <cedricquitor.dev@gmail.com>
2023-02-15 10:46:26 +0800

    server: Added comments for documentations

--- server/routes/systemsRoutes.js
+++ server/routes/systemsRoutes.js
@@ -3,7 +3,17 @@ const router = express.Router();
 const pool = require("../db");
 const { v4: uuidv4 } = require("uuid");
 
-// Get all systems
+/**
+ * Get all systems
+ *
+ * This function retrieves all systems belonging to a specified user email from a database table and sends the resulting rows as a JSON response to the client.
+ *
+ * @function
+ * @async
+ * @param {Object} req - The HTTP request object containing user email in URL parameter.
+ * @param {Object} res - The HTTP response object with response status and message containing queried rows.
+ * @returns {Object} - Returns HTTP response object with status and queried rows in the message.
+ */
 router.route("/:userEmail").get(async (req, res) => {
   console.log(req.params);
   const { userEmail } = req.params;
@@ -16,7 +26,17 @@ router.route("/:userEmail").get(async (req, res) => {
   }
 });
 
-// Add a new system
+/**
+ * Add a new system
+ *
+ * This function inserts a new system, along with system properties, into a database table and returns the resulting rows as a JSON response to the client.
+ *
+ * @function
+ * @async
+ * @param {Object} req - The HTTP request object containing system properties in the request body.
+ * @param {Object} res - The HTTP response object with response status and message containing inserted rows.
+ * @returns {Object} - Returns HTTP response object with status and inserted rows in the message.
+ */
 router.route("/").post(async (req, res) => {
   console.log(req.body);
   const id = uuidv4();
@@ -29,7 +49,17 @@ router.route("/").post(async (req, res) => {
   }
 });
 
-// Edit a system
+/**
+ * Edit a system
+ *
+ * This function updates an existing system, along with its properties, in a database table and returns a JSON response to the client.
+ *
+ * @function
+ * @async
+ * @param {Object} req - The HTTP request object containing the system ID in URL parameter and system properties in the request body.
+ * @param {Object} res - The HTTP response object with response status and message containing updated rows.
+ * @returns {Object} - Returns HTTP response object with status and updated rows in the message.
+ */
 router.route("/:id").put(async (req, res) => {
   console.log(req.params);
   console.log(req.body);
@@ -43,7 +73,17 @@ router.route("/:id").put(async (req, res) => {
   }
 });
 
-// Delete a system
+/**
+ * Delete a system
+ *
+ * This function deletes a system from a database table using the provided ID and returns a JSON response to the client.
+ *
+ * @function
+ * @async
+ * @param {Object} req - The HTTP request object containing the system ID in URL parameter.
+ * @param {Object} res - The HTTP response object with response status and message containing the number of rows deleted.
+ * @returns {Object} - Returns HTTP response object with status and the number of rows deleted in the message.
+ */
 router.route(":/id").delete(async (res, req) => {
   const { id } = req.params;
   try {
