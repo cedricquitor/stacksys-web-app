e3e43872b5da85bbcc115c87587f36d6bbf0b884
cedricquitor <cedricquitor.dev@gmail.com>
2023-03-01 23:16:22 +0800

    client: Init auth component UI

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [isLogin, setIsLogin] = useState(false);
+  const [error, setError] = useState(null);
+
+  const viewLogin = (value) => {
+    setIsLogin(value);
+    setError(false);
+  };
+
+  return (
+    <div>
+      <div>
+        <form action="">
+          <h2>{isLogin ? "Login" : "Register"}</h2>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          {!isLogin ? <input type="password" placeholder="Re-enter password" /> : null}
+          <input type="submit" />
+          {error ? <p>{error}</p> : null}
+        </form>
+        <div>
+          <button onClick={() => viewLogin(false)}>Register</button>
+          <button onClick={() => viewLogin(true)}>Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
